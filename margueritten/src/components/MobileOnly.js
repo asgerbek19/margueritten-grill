77c0483102aca143f72cfff31c3949686a32c80a
asgerbek19 <eaaabmat@students.eaaa.dk>
2022-06-03 01:13:54 +0200

    Updates

--- margueritten/src/components/MobileOnly.js
+++
@@ -1,5 +0,0 @@
-export default function MobileOnly(){
-    return(
-        <div className="mobileonly"><h3>Denne App er kun tilhørende i app formar, åbn den med mobilvisning i developer tools, eller brug din mobil</h3></div>
-    )
-}
